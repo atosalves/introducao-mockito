dc6b328f5cac42869ca4bc4762ddc04c4ddf870d
Atos <atosalvesqueiroz@gmail.com>
2025-02-27 19:36:52 -0300

    test: add integration tests for findTaskById

--- taskmanagement/src/test/java/br/edu/ifpb/taskmanagement/model/repository/TaskRepositoryIntegrationTest.java
+++ taskmanagement/src/test/java/br/edu/ifpb/taskmanagement/model/repository/TaskRepositoryIntegrationTest.java
@@ -3,11 +3,13 @@ package br.edu.ifpb.taskmanagement.model.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 import br.edu.ifpb.taskmanagement.model.entity.TaskEntity;
@@ -18,21 +20,39 @@ import br.edu.ifpb.taskmanagement.model.entity.TaskEntity;
 public class TaskRepositoryIntegrationTest {
 
     @Autowired
+    TestEntityManager testEntityManager;
+
+    @Autowired
     TaskRepository taskRepository;
 
     @Test
     void testPersistInDatabase(){
-        TaskEntity task = new TaskEntity();
+        var task = new TaskEntity();
         task.setTitle("task title");
         task.setDescription("task description");
         task.setCompleted(false);
 
-        TaskEntity savedTask = taskRepository.save(task);
+        var savedTask = taskRepository.save(task);
 
         assertNotNull(savedTask.getId());
         assertEquals(task.getTitle(), "task title");
 
         assertFalse(savedTask.isCompleted());
+    }
+
+    @Test
+    void testFindTaskByIdInDatabase(){
+        var task = new TaskEntity();
+        task.setTitle("task title");
+        task.setDescription("task description");
+        task.setCompleted(false);
+
+        var savedTask = testEntityManager.persist(task);
+
+        var opTask = taskRepository.findById(savedTask.getId());
+
+        assertNotNull(opTask.get());
+        assertEquals(savedTask, opTask.get());
     }
 
 }
